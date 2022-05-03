bbf4460b76a834a50c9e2815058fa6b515b71b24
Nikolin Majmari <nikolinmajmari30@gmail.com>
2022-05-03 21:38:18 +0200

    replaced eta with jsx

--- routes/home.tsx
+++ routes/home.tsx
@@ -1,9 +1,10 @@
-import {Router} from "../deps.ts";
+import {Router,ReactDOMServer,h} from "../deps.ts";
+import Home from "../jsx/home.tsx";
 
 const router = new Router();
 router.get("/",async (ctx,next)=>{
-    console.log("hey papa");
-    await ctx.res.render("./home.eta",{message:"hellow world"});
+    const html = ReactDOMServer.renderToString(Home());
+    return await ctx.res.html(html);
 });
 
 export default router;
